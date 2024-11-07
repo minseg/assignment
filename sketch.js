dcc4bc8a765a7237bb63c6c7450775650f0b4ea7
minseg <artssam7602@naver.com>
2024-11-07 23:42:42 +0900

    바람 효과 추가

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let particles = [];
 let gravity;
+let wind;
 
 function setup() {
   createCanvas(600, 400);
   gravity = createVector(0, 0.1);
+  wind = createVector(0.1, 0);
   for (let i = 0; i < 10; i++) {
     particles.push(new Particle(random(width), random(height), random(10, 20)));
   }
@@ -13,7 +15,9 @@ function draw() {
   background(240);
   for (let particle of particles) {
     particle.applyForce(gravity);
+    particle.applyForce(wind);
     particle.update();
     particle.show();
   }
 }
+
