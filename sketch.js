e961e2072fa319ce46c97f624dabc88e04f84189
minseg <artssam7602@naver.com>
2024-11-07 23:42:22 +0900

    중력 효과 추가

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let particles = [];
+let gravity;
 
 function setup() {
   createCanvas(600, 400);
+  gravity = createVector(0, 0.1);
   for (let i = 0; i < 10; i++) {
     particles.push(new Particle(random(width), random(height), random(10, 20)));
   }
@@ -10,6 +12,7 @@ function setup() {
 function draw() {
   background(240);
   for (let particle of particles) {
+    particle.applyForce(gravity);
     particle.update();
     particle.show();
   }
