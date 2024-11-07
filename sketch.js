4faa7074a5d0892b3b6f752f3265dbde35321a48
minseg <artssam7602@naver.com>
2024-11-07 23:41:42 +0900

    여러개 입자 생성

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let particles = [];
+
 function setup() {
   createCanvas(600, 400);
+  for (let i = 0; i < 10; i++) {
+    particles.push(new Particle(random(width), random(height), random(10, 20)));
+  }
 }
 
 function draw() {
-  background(240);  // 밝은 색 배경으로 변경
+  background(240);
+  for (let particle of particles) {
+    particle.update();
+    particle.show();
+  }
 }
